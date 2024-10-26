6c514368195f80373ca8239c8caf6aaf82dfa2c1
ficolas2 <nicolasdepg@gmail.com>
2024-10-26 19:40:26 +0200

    run: alkenes

--- alkenes/output/run_all.sh
+++ alkenes/output/run_all.sh
@@ -2,7 +2,7 @@
 
 for i in {1..6}; do
   for j in {1..5}; do
-    file="./output/${i}-base${j}.gjf"
+    file="${i}-base${j}.gjf"
     
     if [[ -f $file ]]; then
       echo "Running G09 for $file..."
